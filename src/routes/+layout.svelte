aa22220f3f26047bd60847705f8f6cfc1284d85f
edewindt <edewindt27@outlook.com>
2022-11-02 15:52:21 -0500

    Basic css and layout added

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	import css from '$lib/app.css';
+</script>
+
+<body>
+	<slot />
+</body>
+
+<style>
+	body {
+		min-height: 100vh;
+		height: 100%;
+		width: 100%;
+		background-color: red;
+	}
+</style>
