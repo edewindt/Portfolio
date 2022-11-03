0f05cc4f4348f5aa9c42a62cbd5e575ad0999e9c
edewindt <edewindt27@outlook.com>
2022-11-03 18:05:12 -0500

    Added sound effects to buttons

--- src/routes/normal-ui/+page.svelte
+++ src/routes/normal-ui/+page.svelte
@@ -1,7 +1,11 @@
 <script>
 	import Type from '../../lib/components/type.svelte';
 	import hero from '$lib/assets/lavacomp.png';
-
+	import { Howl, Howler } from 'howler';
+	const buttsound = new Howl({
+		src: ['/src/lib//assets/Electric.wav'],
+		volume: 0.01
+	});
 	let buttons = [
 		{ name: 'Projects', url: '/normal-ui/projects' },
 		{ name: 'Resume', url: '/normal-ui/resume' },
@@ -17,7 +21,17 @@
 	<h2 class="hero-text">My name is Elias and <Type /></h2>
 	<div id="buttons">
 		{#each buttons as { name, url }}
-			<a href={url}><button class="butt">{name}</button></a>
+			<a href={url}
+				><button
+					class="butt"
+					on:focus={() => {
+						buttsound.play();
+					}}
+					on:mouseover={() => {
+						buttsound.play();
+					}}>{name}</button
+				></a
+			>
 		{/each}
 	</div>
 </section>
