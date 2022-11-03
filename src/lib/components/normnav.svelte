dd6f49b37b892bead0c6a9c8f1822f247b16380a
edewindt <edewindt27@outlook.com>
2022-11-03 12:47:39 -0500

    Added links to nav

--- src/lib/components/normnav.svelte
+++ src/lib/components/normnav.svelte
@@ -1,1 +1,30 @@
-<h2>Hello</h2>
+<script>
+	let pr = '/normal-ui';
+	let nav = [
+		{ url: pr, name: 'Home' },
+		{ url: pr + '/contact', name: 'Contact' },
+		{ url: pr + '/design', name: 'Design' },
+		{ url: pr + '/projects', name: 'Projects' },
+		{ url: pr + '/resume', name: 'Resume' }
+	];
+</script>
+
+<nav>
+	<ul>
+		{#each nav as { url, name }}
+			<li><a href={url}>{name}</a></li>
+		{/each}
+	</ul>
+</nav>
+
+<style>
+	nav {
+		width: 100%;
+		display: flex;
+		justify-content: center;
+	}
+	ul {
+		display: flex;
+		justify-content: space-between;
+	}
+</style>
