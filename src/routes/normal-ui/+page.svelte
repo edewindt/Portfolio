91db5dbac204834f82927c2270a0441194a30ab0
edewindt <edewindt27@outlook.com>
2022-11-03 16:47:35 -0500

    fixed img placement

--- src/routes/normal-ui/+page.svelte
+++ src/routes/normal-ui/+page.svelte
@@ -5,16 +5,24 @@
 <section
 	class="hero"
 	style="  background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url({hero});"
-/>
+>
+	<h2 class="hero-text">My name is Elias</h2>
+</section>
 
 <style>
 	.hero {
 		height: 100vh;
 		background-size: cover;
+		background-position: center;
+		background-repeat: no-repeat;
 	}
-	.hero::before {
-		content: '';
+	.hero-text {
+		text-align: center;
 		position: absolute;
-		top: 0px;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		color: white;
+		font-size: 2rem;
 	}
 </style>
