4606911dbf4fbc616f861306b0c25dac5acb8900
edewindt <edewindt27@outlook.com>
2022-11-04 10:21:28 -0500

    Formatted form

--- src/routes/normal-ui/contact/+page.svelte
+++ src/routes/normal-ui/contact/+page.svelte
@@ -1,30 +1,45 @@
-<h1>Contact</h1>
+<section>
+	<h1>Contact</h1>
 
-<section class="contact">
-	<p>Thank you for taking the time and energy to contact me.</p>
-	<p>To contact me through Social Media use the following links:</p>
-	<ul>
-		<li><a href="https://www.linkedin.com/in/eliasdewindt/">LinkedIn</a></li>
-		<li><a href="https://edewindt.com/contact-me/">My Blog (edewindt.com)</a></li>
-	</ul>
-	<p>For general questions and contact, feel free to email me at elias@edewindt.com</p>
-	<p>
-		If you want to work with me on a project, or idea, I would gladly consider working with you. To
-		see the full range of skills I posses, visit my Resume page.
-	</p>
-	<p>
-		Please fill out the form below and we’ll be in touch. I will attempt to respond in 24 hours or
-		less!
-	</p>
+	<div class="contact">
+		<p>Thank you for taking the time and energy to contact me.</p>
+		<p>To contact me through Social Media use the following links:</p>
+		<ul>
+			<li><a href="https://www.linkedin.com/in/eliasdewindt/">LinkedIn</a></li>
+			<li><a href="https://edewindt.com/contact-me/">My Blog (edewindt.com)</a></li>
+		</ul>
+		<p>For general questions and contact, feel free to email me at elias@edewindt.com</p>
+		<p>
+			If you want to work with me on a project, or idea, I would gladly consider working with you.
+			To see the full range of skills I posses, visit my Resume page.
+		</p>
+		<p>
+			Please fill out the form below and we’ll be in touch. I will attempt to respond in 24 hours or
+			less!
+		</p>
 
-	<form action="">
-		<label for="">Name</label><input type="text" /><label for="">Email</label><input
-			type="text"
-		/><label for="">Phone</label><input type="text" /><label for="">Message</label><textarea
-			name=""
-			id=""
-			cols="30"
-			rows="10"
-		/>
-	</form>
+		<form action="">
+			<label for="">Name</label><input type="text" /><label for="">Email</label><input
+				type="text"
+			/><label for="">Phone</label><input type="text" /><label for="">Message</label><textarea
+				name=""
+				id=""
+				cols="30"
+				rows="10"
+			/>
+		</form>
+	</div>
 </section>
+
+<style>
+	.contact {
+		display: flex;
+		align-items: center;
+		flex-direction: column;
+	}
+	.contact form {
+		display: flex;
+		align-items: center;
+		flex-direction: column;
+	}
+</style>
