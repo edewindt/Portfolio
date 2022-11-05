bd4b172ea48855d394a30983eb50fe94ea85b8d9
edewindt <edewindt27@outlook.com>
2022-11-05 06:34:26 -0500

    Changed contact form a little

--- src/routes/normal-ui/contact/+page.svelte
+++ src/routes/normal-ui/contact/+page.svelte
@@ -10,32 +10,36 @@
 		<h1>Contact</h1>
 
 		<div class="contact">
-			<p>Thank you for taking the time and energy to contact me.</p>
-			<p>To contact me through Social Media use the following links:</p>
-			<ul>
-				<li><a href="https://www.linkedin.com/in/eliasdewindt/">LinkedIn</a></li>
-				<li><a href="https://edewindt.com/contact-me/">My Blog (edewindt.com)</a></li>
-			</ul>
-			<p>
-				For general questions and contact, feel free to email me at <span class="email"
-					>elias@edewindt.com</span
-				>
-			</p>
-			<p>
-				If you want to work with me on a project, or idea, I would gladly consider working with you.
-				To see the full range of skills I posses, visit my Resume page.
-			</p>
-			<p>
-				Please fill out the form below and we’ll be in touch. I will attempt to respond in 24 hours
-				or less!
-			</p>
+			<div class="paragraphs">
+				<p>Thank you for taking the time and energy to contact me.</p>
+				<p>To contact me through Social Media use the following links:</p>
+				<ul>
+					<li><a href="https://www.linkedin.com/in/eliasdewindt/">LinkedIn</a></li>
+					<li><a href="https://edewindt.com/contact-me/">My Blog (edewindt.com)</a></li>
+				</ul>
+				<p>
+					For general questions and contact, feel free to email me at <span class="email"
+						>elias@edewindt.com</span
+					>
+				</p>
+				<p>
+					If you want to work with me on a project, or idea, I would gladly consider working with
+					you. To see the full range of skills I posses, visit my Resume page.
+				</p>
+				<p>
+					Please fill out the form and we’ll be in touch. I will attempt to respond in 24 hours or
+					less!
+				</p>
+			</div>
 
 			<form action="">
-				<label for="">Name (Required)</label><input type="text" /><label for=""
-					>Email (Required)</label
-				><input type="text" /><label for="">Phone</label><input type="text" /><label for=""
-					>Message</label
-				><textarea name="" id="" cols="30" rows="10" />
+				<label for="">Name (Required)</label><input type="text" />
+
+				<label for="">Email (Required)</label><input type="text" />
+
+				<label for="">Phone</label><input type="text" />
+
+				<label for="">Message</label><textarea name="" id="" cols="30" rows="10" />
 			</form>
 		</div>
 	</section>
@@ -44,6 +48,7 @@
 <style>
 	h1 {
 		color: white;
+		font-size: 2.5rem;
 	}
 	.cont {
 		height: calc(100vh - 3.5rem);
@@ -62,12 +67,13 @@
 	.contact {
 		display: flex;
 		align-items: center;
-		width: 40rem;
+		width: 70rem;
 		max-width: 100%;
-		flex-direction: column;
 		background-color: var(--dark);
-		padding: 1rem;
+		padding: 4rem;
 		color: var(--light);
+		padding-bottom: 3.5rem;
+		font-size: 20px;
 	}
 	.contact form {
 		display: flex;
@@ -75,12 +81,23 @@
 		flex-direction: column;
 	}
 	input {
-		width: 40rem;
+		width: 30rem;
+		height: 1.5rem;
 		max-width: calc(100vw - 5rem);
+		background-color: pink;
+		border: none;
+	}
+	label {
+		margin-top: 2rem;
+		display: flex;
+		justify-content: start;
+		width: 100%;
 	}
 	textarea {
-		width: 40rem;
+		width: 30rem;
 		max-width: calc(100vw - 5rem);
+		background-color: pink;
+		border: none;
 	}
 	.contact p {
 		margin: 1rem;
@@ -90,5 +107,9 @@
 	}
 	.email {
 		color: pink;
+	}
+	ul {
+		position: relative;
+		left: 5rem;
 	}
 </style>
