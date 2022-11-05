77f0e8e45b01344636c54fe528aa786c7ad1ab41
edewindt <edewindt27@outlook.com>
2022-11-05 07:26:49 -0500

    Added backend card

--- src/routes/normal-ui/projects/+page.svelte
+++ src/routes/normal-ui/projects/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import reactlogo from '$lib/assets/react.png';
+	import nodelogo from '$lib/assets/node.png';
 	import ProCard from '../../../lib/components/proCard.svelte';
 	const categories = [
 		{
@@ -8,8 +9,17 @@
 				'The HTML, CSS, and JavaScript, Everything that makes up the UI, including Libraies and Frameworks',
 			img: reactlogo
 		},
-		{ name: 'Games', description: '', img: '' },
-		{ name: 'Back-end', description: '', img: '' }
+		{
+			name: 'Games',
+			description: 'For experimentation, and fun, but building games also teaches me a lot',
+			img: ''
+		},
+		{
+			name: 'Back-end',
+			description:
+				'The stuff that no one sees or appreciates that makes an application truly complex',
+			img: nodelogo
+		}
 	];
 </script>
 
