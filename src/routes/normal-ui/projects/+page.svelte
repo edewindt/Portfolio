8927aaa69725e937c0e392c871dde5564a415346
edewindt <edewindt27@outlook.com>
2022-11-05 08:04:17 -0500

    Background added

--- src/routes/normal-ui/projects/+page.svelte
+++ src/routes/normal-ui/projects/+page.svelte
@@ -2,6 +2,7 @@
 	import reactlogo from '$lib/assets/react.png';
 	import nodelogo from '$lib/assets/node.png';
 	import gamelogo from '$lib/assets/game.png';
+	import water from '$lib/assets/water.png';
 	import ProCard from '../../../lib/components/proCard.svelte';
 	const categories = [
 		{
@@ -24,18 +25,31 @@
 	];
 </script>
 
-<section>
-	<h2>Projects</h2>
-	<div class="categories">
-		{#each categories as { name, description, img }}
-			<ProCard {name} {description} {img} />
-		{/each}
-	</div>
-</section>
+<div
+	class="cont"
+	style="background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url({water});"
+>
+	<section>
+		<h2>Projects</h2>
+		<div class="categories">
+			{#each categories as { name, description, img }}
+				<ProCard {name} {description} {img} />
+			{/each}
+		</div>
+	</section>
+</div>
 
 <style>
+	.cont {
+		height: calc(100vh - 3.5rem);
+		background-size: cover;
+		background-position: center;
+		background-repeat: no-repeat;
+	}
 	h2 {
 		font-size: 3rem;
+		color: var(--light);
+		margin-bottom: 2rem;
 	}
 	section {
 		display: flex;
