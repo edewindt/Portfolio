e5d76a664a66d6bc1fb67a85517066b0e3762699
edewindt <edewindt27@outlook.com>
2022-11-07 13:29:54 -0600

    Started resume page

--- src/routes/normal-ui/resume/+page.svelte
+++ src/routes/normal-ui/resume/+page.svelte
@@ -1,1 +1,40 @@
-<h1>Resume</h1>
+<script>
+	import hero from '$lib/assets/lavacomp.png';
+</script>
+
+<section
+	class="cont"
+	style="  background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url({hero});"
+>
+	<div class="inner">
+		<h2 class="hero-text">Resume</h2>
+		<h3>Profile</h3>
+		<p>
+			I like to break people's expectations, and learn everything I can about anything I'm
+			interested in.
+		</p>
+	</div>
+</section>
+
+<style>
+	.cont {
+		height: fit-content;
+		min-height: 100vh;
+		background-size: cover;
+		background-position: center;
+		background-repeat: no-repeat;
+		justify-content: center;
+		flex-direction: column;
+	}
+	.hero-text {
+		color: white;
+		font-size: 4rem;
+		text-align: center;
+	}
+	h3 {
+		color: var(--light);
+	}
+	p {
+		color: var(--light);
+	}
+</style>
