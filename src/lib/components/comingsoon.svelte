297894a0b7b1181115d1a312c555df2860661b2e
edewindt <edewindt27@outlook.com>
2022-11-08 16:06:49 -0600

    Styled coming soon

--- src/lib/components/comingsoon.svelte
+++ src/lib/components/comingsoon.svelte
@@ -1,9 +1,20 @@
 <section>
 	<p>Coming Soon...</p>
+	<p>Double tap or press ESC</p>
 </section>
 
 <style>
 	p {
 		color: black;
+		font-size: 3rem;
+		text-align: center;
+	}
+	section {
+		display: flex;
+		justify-content: center;
+		flex-direction: column;
+		gap: 2rem;
+		align-items: center;
+		height: calc(100vh - 3.5rem);
 	}
 </style>
