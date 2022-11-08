90ce2504d7db863a5326f2d37b63d5aef8e54a7f
edewindt <edewindt27@outlook.com>
2022-11-08 16:25:58 -0600

    Changed file name to prevent bug

--- src/routes/normal-ui/projects/back-end/+page.svelte
+++ src/routes/normal-ui/projects/back-end/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-	import got from '$lib/assets/GoT API.png';
+	import got from '$lib/assets/GoTAPI.png';
 	import water from '$lib/assets/water.png';
 	import Backend from '../../../../lib/components/backend.svelte';
 	const categories = [
