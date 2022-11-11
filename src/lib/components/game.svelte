26a94b40ff955e168a672115c2908c27b9ae340b
edewindt <edewindt27@outlook.com>
2022-11-10 21:35:34 -0600

    Game component created

---
+++ src/lib/components/game.svelte
@@ -0,0 +1,5 @@
+<script>
+	import map from '$lib/assets/game/Pokemon Mapz.png';
+</script>
+
+<img src={map} alt="" srcset="" />
--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,1 +1,6 @@
+<script>
+	import Game from '../../lib/components/game.svelte';
+</script>
+
 <h1>Test Page</h1>
+<Game />
